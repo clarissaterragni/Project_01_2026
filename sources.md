16e55ca9e8f8c257c478a040f0251f25f90c44c0
clarissaterragni <clarissa.terragni@mail.polimi.it>
2026-01-15 10:56:15 +0100

    Considerations+computation

---
+++ sources.md
@@ -0,0 +1,2 @@
+https://www.vhdl-online.de/courses/system\_design/vhdl\_language\_and\_syntax/data\_types#data\_type\_time
+
--- timer.vhd
+++ timer.vhd
@@ -11,23 +11,32 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.math_real.all;
 
-entity timer is
+entity timer is                         -- Count a specific duration based on clock frequency and time generic.
 
 generic (
-            clk_freq_hz_g : natural;   -- Clock frequency [Hz]
+            clk_freq_hz_g : natural;    -- Clock frequency [Hz]
                                         -- Natural: non negative integers
 
             delay_g : time              -- Delay duration
                                         -- Time: type consisting of numerical value + physical unit
         );
-port (
+port    (
             clk_i : in std_ulogic;
             arst_i : in std_ulogic;
             start_i : in std_ulogic;    -- No effect if not done_o
             done_o : out std_ulogic     -- ’1’ when not counting (" not busy ")
-     );
+        );
 
 end entity timer ;
 
--- ’time’ signals can be multiplied/divided by integer or real values. Operation's result is again a ’time’ type
--- Source: https://www.vhdl-online.de/courses/system_design/vhdl_language_and_syntax/data_types#data_type_time
+architecture Behavioral of timer is
+
+        constant delay_real    : real := real(delay_g) / 1 sec;                 -- VHDL stores time-type as integer in fs. Transf in seconds 
+                                                                                -- in order to multiply for clock (Hz)
+                                                                                -- ’time’ can be multiplied/divided by integer or real values
+                                                                                -- so cast as real (from vhdl-online.de). Do same for frequency
+        constant freq_real     : real := real(clk_freq_hz_g);
+        constant counter_limit : integer := integer(freq_real * delay_real);    -- Get integer cause it's n° clock cycles
+
+
+end Behavioral;
