befb49f79f6a4da5769219681cac77bfe5154e8a
clarissaterragni <clarissa.terragni@mail.polimi.it>
2026-01-16 12:30:26 +0100

    process counting in timer

--- timer.vhd
+++ timer.vhd
@@ -35,8 +35,38 @@ architecture Behavioral of timer is
                                                                                 -- in order to multiply for clock (Hz)
                                                                                 -- ’time’ can be multiplied/divided by integer or real values
                                                                                 -- so cast as real (from vhdl-online.de). Do same for frequency
-        constant freq_real     : real := real(clk_freq_hz_g);
+
+        constant freq_real     : real := real(clk_freq_hz_g);                   -- Constant cause generics fixed per instance (don't change at runtime)
+
         constant counter_limit : integer := integer(freq_real * delay_real);    -- Get integer cause it's n° clock cycles
 
+        signal counter_value   : integer range 0 to counter_limit;
+        signal not_counting    : std_ulogic;
 
+begin
+
+        done_o <= not_counting;         -- if counting '0', if not counting '1'
+
+        counting : process(clk_i) begin
+
+                if rising_edge(clk_i) then        
+
+                        if (not_counting = '1') then                            -- wasn't already counting
+                                counter_value <= 0;                             -- reset counter to 0
+
+                                if (start_i) = '1' then                         -- start_i sampled on rising edges of clk
+                                        not_counting <= '0';                    -- Counting starts on clock cycle following start_i high
+                                end if;
+
+                        else                                                    -- already counting
+                                if (counter_value < counter_limit) then
+                                        counter_value <= counter_value + 1;     -- increment counter
+                                else
+                                        counter_value <= 0;                     -- end of delay period
+                                        not_counting <= '1';                    -- signal to output done_o       
+                                end if;
+                        end if;
+                end if;
+
+        end process counting;
 end Behavioral;
