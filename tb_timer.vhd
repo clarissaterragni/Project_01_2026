909d3cf040c110ad9cfbe6d7f7898d3d2d2d6dee
clarissaterragni <clarissa.terragni@mail.polimi.it>
2026-01-20 00:08:32 +0100

    tb 1Mhz 100ms

--- tb_timer.vhd
+++ tb_timer.vhd
@@ -7,8 +7,8 @@ context vunit_lib.vunit_context;
 
 entity tb_timer is
     generic (
-      clk_freq_hz_g : natural := 200_000;
-      delay_g       : time    := 40 us;
+      clk_freq_hz_g : natural := 1_000_000;
+      delay_g       : time    := 100 ms;
       runner_cfg    : string  := ""
     );
 end entity tb_timer;
