2d407b1787248e4a428481af50a4995b725aec0b
clarissaterragni <clarissa.terragni@mail.polimi.it>
2026-01-19 19:15:11 +0100

    debugging

--- run.py
+++ run.py
@@ -1,17 +1,7 @@
-import vunit.sim_if.ghdl._ghdl as _ghdl
-
-if hasattr(_ghdl, "GHDLBackend") and hasattr(_ghdl.GHDLBackend, "_backend_mapping"):
-    _original_mapping = _ghdl.GHDLBackend._backend_mapping.copy()
-    _ghdl.GHDLBackend._backend_mapping = {
-        **_original_mapping,
-        r"mcode JIT code generator": "mcode",
-    }
-
-
 from vunit import VUnit
 
-vu = VUnit.from_argv(compile_builtins=False)
-vu.add_vhdl_builtins()
+vu = VUnit.from_argv()
+
 
 lib = vu.add_library("lib")
 lib.add_source_files("timer.vhd")
