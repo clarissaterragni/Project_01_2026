8c35a8227bf6765bb10f60e975b85677a2b47471
clarissaterragni <clarissa.terragni@mail.polimi.it>
2026-01-19 23:03:46 +0100

    debugging

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from vunit import VUnit
 
-vu = VUnit.from_argv()
-
+vu = VUnit.from_argv(compile_builtins=False)
+vu.add_vhdl_builtins()
 
 lib = vu.add_library("lib")
 lib.add_source_files("timer.vhd")
